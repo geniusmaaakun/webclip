f26e3e3127430fda0845ebc16bfe7c436b75c50c
geniusmaaakun <ao_masa_0610@yahoo.co.jp>
2023-05-09 21:01:43 +0900

    add code

--- frontend/src/__tests__/App.test.tsx
+++ frontend/src/__tests__/App.test.tsx
@@ -12,7 +12,7 @@ test('renders learn react link', () => {
 
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+import App from "../App";
 
 test("renders learn react link", () => {
   render(<App />);
