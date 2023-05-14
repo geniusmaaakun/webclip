044a00da274c169523516ab144428eacaf8f9db9
geniusmaaakun <ao_masa_0610@yahoo.co.jp>
2023-05-15 07:20:19 +0900

    add code

--- main.go
+++ main.go
@@ -1,29 +1,28 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"webclip/src/actions"
-	"webclip/src/server/models"
 )
 
 //このコードでは、`urfave/cli/v2`ライブラリを使用してコマンドラインオプションを定義し、`Action`関数で処理を実行しています。このライブラリを使用することで、コマンドラインオプションの定義が簡潔になり、エラーチェックやヘルプメッセージの生成などが自動化されます。
 //上記のコードでは、`HTMLToMarkdownConverter`と`wcdownloader`という構造体を使用して、処理を行っています。これにより、コードがより構造化され、プロのエンジニアが書いたようなスタイルになっています。
 
 func main() {
-	folderPath, err := models.GetDatabasePath()
-	if err != nil {
-		log.Fatalf("main: %v\n", err)
-	}
 	//production
+	// folderPath, err := models.GetDatabasePath()
+	// if err != nil {
+	// 	log.Fatalf("main: %v\n", err)
+	// }
+	// fmt.Println(folderPath)
+
 	//app := actions.NewWebClip(filepath.Join(folderPath, "webclip.sql"))
 
 	//development
-	fmt.Println(folderPath)
 	app := actions.NewWebClip("webclip.sql")
 
-	err = app.Run(os.Args)
+	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
